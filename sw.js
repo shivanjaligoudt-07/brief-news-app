c09e19c248f3d0d1a5e2d59b7a5e2061f04ff358
shivanjaligoudt-07 <shivanjaligoud.t@gmail.com>
2026-04-20 21:50:10 +0530

    Create sw.js file for service worker offline support

---
+++ sw.js
@@ -0,0 +1,13 @@
+'use strict';
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetching: ', event.request.url);
+});
